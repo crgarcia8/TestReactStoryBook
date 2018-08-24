2154fc64d0fc137fdc70940514e5517d34ef0941
crgarcia8 <crgarcia8@gmail.com>
2018-08-24 16:58:55 -0500

    fix2

--- src/components/test.js
+++ src/components/test.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Test = (props) => (
-    <div>testtttt</div>
+    <div>
+        <Link to={`/`}>
+            Volver a Login
+        </Link>
+    </div>
 )
 
 export default Test;
