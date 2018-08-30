3693cc97f837f57a00dba654fe9c0049c052b81d
RaulMarz <raul.martinez@publicisone.com>
2018-08-30 11:27:29 -0500

    fix

--- src/components/clientGrid.js
+++ src/components/clientGrid.js
@@ -6,34 +6,41 @@ import {fetchClients} from '../actions';
 class ClientGrid extends Component {
   render() {
     return (
-      <div className="md ">
-        <ReactTable
-          data = {this.props.clients}
-          columns={[
-            {
-              Header: "CLIENT NAME",
-              accessor: "name"
-            },
-            {
-              Header: "STATUS",
-              accessor: "status"
-            },
-            {
-              Header: 'CREATED DATE',
-              accessor: "dateCreation"
-            },
-            {
-              Header: 'LAST MODIFIED DATE',
-              accessor: "dateModified"
-            },
-            {
-              Header: '',
-              accessor: "menu"
-            }
-          ]}
-          defaultPageSize={10}
-          className="-striped -highlight"
-        />
+      <div className="row">
+        <div class="clientGrid small">
+          <ReactTable
+            data = {this.props.clients}
+            columns={[
+              {
+                Header: "CLIENT NAME",
+                accessor: "name",
+                minWidth: 200,
+                style:{}
+              },
+              {
+                Header: "STATUS",
+                accessor: "status"
+              },
+              {
+                Header: 'CREATED DATE',
+                accessor: "dateCreation"
+              },
+              {
+                Header: 'LAST MODIFIED DATE',
+                accessor: "dateModified"
+              },
+              {
+                Header: '',
+                accessor: "menu",
+                width: 25
+              }
+            ]}
+            defaultPageSize={10}
+            className="-highlight"
+            noDataText="Create your first client"
+            showPageSizeOptions="false"
+          />
+        </div>
       </div>
     );
   }
