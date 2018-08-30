b3520093c93d3d8f474f62f1c5211b51da825e4b
RaulMarz <raul.martinez@publicisone.com>
2018-08-30 12:21:57 -0500

    fix

--- src/components/clientGrid.js
+++ src/components/clientGrid.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import ReactTable from "react-table";
-import { connect } from 'react-redux';
-import { fetchClients } from '../actions';
+import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class ClientGrid extends Component {
   render() {
     return (
+      <div>
+      <div className="row">
+        <span className="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+      </div>
       <div className="row">
         <div className="clientGrid small">
           <ReactTable
-            data={this.props.clients}
+            data = {this.props.clients}
             columns={[
               {
                 Header: "CLIENT NAME",
                 accessor: "name",
                 minWidth: 200,
-                style: {}
+                style:{}
               },
               {
                 Header: "STATUS",
@@ -31,24 +35,37 @@ class ClientGrid extends Component {
               },
               {
                 Header: '',
-                accessor: "menu",
-                width: 25
+                width: 25,
+
+                Cell: row => (
+                  <div>
+                    <Link className="customLink" to={`/clientDetail`}>
+                      ...
+                      </Link>
+                  </div>
+                )
+
               }
             ]}
             defaultPageSize={10}
             className="-highlight"
             noDataText="Create your first client"
-                      />
+            showPageSizeOptions={false}
+            sortable={true}
+            nextText=">"
+            previousText="<"
+          />
         </div>
+      </div>
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    clients: state.clients
+function mapStateToProps(state){
+  return{
+      clients: state.clients
   };
 }
 
-export default connect(mapStateToProps)(ClientGrid);
+export default connect (mapStateToProps)(ClientGrid);
